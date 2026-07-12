bda8e00f91d5a5d1c61247501d081ca792b60496
Karan Singh Rajput <karansinghrajput2022@vitbhopal.ac.in>
2026-07-12 17:45:00 +0530

    Complete Angular Hands-on 2 - Data Binding and Component Communication

--- week1/student-course-portal/src/app/pages/home/home.ts
+++ week1/student-course-portal/src/app/pages/home/home.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class Home implements OnInit, OnDestroy {
+
+  portalName = 'Student Course Portal';
+  isPortalActive = true;
+  message = '';
+  searchTerm = '';
+
+  coursesAvailable = 12;
+
+  onEnrollClick() {
+    this.message = 'Enrollment opened!';
+  }
+
+  ngOnInit() {
+    console.log('HomeComponent initialized - courses loaded');
+  }
+
+  ngOnDestroy() {
+    console.log('HomeComponent destroyed');
+  }
+}
